01712739498b69ac131da9fd39f343d2357cbfb4
walaz05 <walaz05@users.noreply.github.com>
2026-02-10 19:48:59 -0500

    Hide App source code from public view

--- Android/app/src/main/java/com/katchy/focuslive/widget/PomodoroWidgetReceiver.kt
+++
@@ -1,8 +0,0 @@
-package com.katchy.focuslive.widget
-
-import androidx.glance.appwidget.GlanceAppWidget
-import androidx.glance.appwidget.GlanceAppWidgetReceiver
-
-class PomodoroWidgetReceiver : GlanceAppWidgetReceiver() {
-    override val glanceAppWidget: GlanceAppWidget = PomodoroWidget()
-}
--- Android/app/src/main/java/com/katchy/focuslive/widget/TasksWidgetReceiver.kt
+++
@@ -1,8 +0,0 @@
-package com.katchy.focuslive.widget
-
-import androidx.glance.appwidget.GlanceAppWidget
-import androidx.glance.appwidget.GlanceAppWidgetReceiver
-
-class TasksWidgetReceiver : GlanceAppWidgetReceiver() {
-    override val glanceAppWidget: GlanceAppWidget = TasksWidget()
-}
